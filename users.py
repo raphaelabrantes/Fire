c01a47c0c40124fa932c952a08a4505dbca25ad9
raphaelabrantes <antenabr2@gmail.com>
2019-10-10 18:35:28 -0300

    The method remove_user_n now uses the new method in portas.py

--- users.py
+++ users.py
@@ -1,10 +1,11 @@
 import mysql.connector
-from portas import remove_port_ip
+from portas import remove_total_ip
+
 
 def add_user(cursor, mydb):
+    query = "INSERT INTO Usuarios(name, ip) VALUES(%s, INET_ATON(%s))"
     user = input("Name: ")
     ip = input("IP: ")
-    query = "INSERT INTO Usuarios(name, ip) VALUES(%s, INET_ATON(%s))"
     cursor.execute(query, (user, ip))
     mydb.commit()
     print(cursor.rowcount)
@@ -19,28 +20,28 @@ def show_users(cursor, mydb):
 
 
 def remove_user_n(cursor, mydb):
+    query_s = "SELECT Portas.ip FROM Portas INNER JOIN Usuarios ON Portas.ip=Usuarios.ip WHERE Usuarios.name=%s LIMIT 1"
+    query_d = "DELETE FROM Usuarios WHERE name=%s"
     user = input("Name: ")
-    query1 = "SELECT Portas.ip, Portas.port FROM Portas INNER JOIN Usuarios ON Portas.ip=Usuarios.ip WHERE Usuarios.name=%s"
-    cursor.execute(query1, (user, ))
+    cursor.execute(query_s, (user, ))
     reply = cursor.fetchall()
-    for ip, port in reply:
-        remove_port_ip(cursor, mydb, [ip, port])
-    query = "DELETE FROM Usuarios WHERE name=%s"
-    cursor.execute(query, (user,))
+    for ip in reply:
+        remove_total_ip(cursor, mydb, ip)
+    cursor.execute(query_d, (user,))
     mydb.commit()
 
 
 def remove_user_ip(cursor, mydb):
-    ip = input("Ip: ")
     query = "DELETE FROM Usuarios WHERE ip=INET_ATON(%s)"
+    ip = input("Ip: ")
     cursor.execute(query, (ip,))
     mydb.commit()
 
 
 def search_user(cursor, mydb):
+    query = "SELECT name, INET_NTOA(ip) as IP FROM Usuarios WHERE name=%s OR ip=INET_ATON(%s)"
     name = input("Name: ")
     ip = input("Ip: ")
-    query = "SELECT name, INET_NTOA(ip) as IP FROM Usuarios WHERE name=%s OR ip=INET_ATON(%s)"
     cursor.execute(query, (name, ip))
     reply = cursor.fetchall()
     for name, ip in reply:
