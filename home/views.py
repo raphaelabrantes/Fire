77b72cef93282f5f90001c05fdb574fc963b8a82
raphaelabrantes <antenabr2@gmail.com>
2019-10-23 15:58:20 -0300

    adding loggin function

--- home/views.py
+++ home/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
-    return HttpResponse("Ola Nicolle")
+    return render(request, 'home/index.html')
+
+
+def log(request):
+    if 'username' in request.POST and 'password' in request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
+    return HttpResponse("Nao vai da")
