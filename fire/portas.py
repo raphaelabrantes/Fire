ae619cb784d4a09a09c8c5bc2299fe4ddf73c1c6
raphaelabrantes <antenabr2@gmail.com>
2020-02-20 18:03:06 -0300

    Changed all output to firecli object output and changed the inserts in the table

--- fire/portas.py
+++ fire/portas.py
@@ -2,9 +2,9 @@ import mysql.connector
 import os
 
 
-def add_port(cursor, mydb):
-    query = "INSERT INTO Portas(ip, port) VALUES(INET_ATON(%s), %s)"
-    show_ports(cursor, mydb)
+def add_port(cursor, mydb, obj):
+    query = "INSERT INTO Portas(ip, port, date_add) VALUES(INET_ATON(%s), %s, CURRENT_TIMESTAMP )"
+    show_ports(cursor, mydb, obj)
     ip = input("IP: ")
     port = input("Port: ")
     try:
@@ -13,27 +13,27 @@ def add_port(cursor, mydb):
         command = "ufw allow from {}  to any port {}".format(ip, port)
     except mysql.connector.errors.IntegrityError as e:
         # TODO verificar exatamente o erro
-        print("Port already open to this IP")
+        obj.stdout.write("Port already open to this IP")
 
     except mysql.connector.errors.DatabaseError as e:
-        print("Data is not in right format")
+        obj.stdout.write("Data is not in right format")
     else:
         reply = list(os.popen(command))
-        print(reply[0])
+        obj.stdout.write(reply[0])
 
 
-def show_ports(cursor, mydb):
+def show_ports(cursor, mydb, obj):
     query = "SELECT INET_NTOA(Portas.ip), port, Portas.date_add , name FROM Portas INNER JOIN Usuarios U on Portas.ip = U.ip"
     cursor.execute(query)
     reply = cursor.fetchall()
     for ip, port, data, name in reply:
-        print("%s\t\t%s\t%s\t%s" % (ip, port, data.strftime("%m/%d/%Y, %H:%M:%S"), name))
+        obj.stdout.write("%s\t\t%s\t%s\t%s" % (ip, port, data.strftime("%m/%d/%Y, %H:%M:%S"), name))
 
 
-def remove_ports(cursor, mydb):
+def remove_ports(cursor, mydb, obj):
     query_s = "SELECT INET_NTOA(ip) FROM Portas WHERE port=%s"
     query_d = "DELETE FROM Portas WHERE port=%s"
-    show_ports(cursor, mydb)
+    show_ports(cursor, mydb, obj)
     port = input("Port: ")
     string = "ufw delete allow from {} to any port {}"
     cursor.execute(query_s, (port,))
@@ -43,23 +43,23 @@ def remove_ports(cursor, mydb):
     for ip in select:
         command = string.format(ip[0], port)
         reply = list(os.popen(command))
-        print(reply)
+        obj.stdout.write(reply)
 
 
-def remove_port_ip(cursor, mydb, query=None):
+def remove_port_ip(cursor, mydb, obj, query=None):
     query = "DELETE FROM Portas WHERE ip=INET_ATON(%s) AND port=%s"
-    show_ports(cursor, mydb)
+    show_ports(cursor, mydb, obj)
     ip = input("Ip: ")
     port = input("Port: ")
     command = "ufw delete allow from {} to any port {}".format(ip, port)
     reply = list(os.popen(command))
-    print(reply[0])
+    obj.stdout.write(reply[0])
     cursor.execute(query, (ip, port))
     mydb.commit()
 
 
-def remove_total_ip(cursor, mydb, ip=None):
-    show_ports(cursor, mydb)
+def remove_total_ip(cursor, mydb, obj, ip=None):
+    show_ports(cursor, mydb, obj)
     query_s = "SELECT port FROM Portas WHERE ip=INET_ATON(%s)"
     query_d = "DELETE FROM Portas WHERE ip=INET_ATON(%s)"
     if not ip:
@@ -70,16 +70,16 @@ def remove_total_ip(cursor, mydb, ip=None):
     for port in reply:
         command = string.format(ip, port[0])
         reply = list(os.popen(command))
-        print(reply)
+        obj.stdout.write(reply)
     cursor.execute(query_d, (ip,))
     mydb.commit()
 
 
-def search_port(cursor, mydb):
+def search_port(cursor, mydb, obj):
     query = "SELECT port, INET_NTOA(ip) as IP FROM Portas WHERE port=%s OR ip=INET_ATON(%s)"
     port = input("Port: ")
     ip = input("Ip: ")
     cursor.execute(query, (port, ip))
     reply = cursor.fetchall()
     for port, ip in reply:
-        print(port, ip)
+        obj.stdout.write(port, ip)
--- fire/users.py
+++ fire/users.py
@@ -1,53 +1,54 @@
 from fire.portas import remove_total_ip
 
 
-def add_user(cursor, mydb):
-    show_users(cursor, mydb)
-    query = "INSERT INTO Usuarios(name, ip) VALUES(%s, INET_ATON(%s))"
+def add_user(cursor, mydb, obj):
+    show_users(cursor, mydb, obj)
+    query = "INSERT INTO Usuarios(name, ip, date_add) VALUES(%s, INET_ATON(%s), CURRENT_TIMESTAMP )"
     user = input("Name: ")
     ip = input("IP: ")
     cursor.execute(query, (user, ip))
     mydb.commit()
-    print(cursor.rowcount)
+    obj.stdout.write(str(cursor.rowcount))
 
 
-def show_users(cursor, mydb):
+def show_users(cursor, mydb, obj):
     query = "SELECT name, INET_NTOA(ip), date_add FROM Usuarios"
     cursor.execute(query)
     reply = cursor.fetchall()
     for nome, ip, data in reply:
-        print("%s\t\t%s\t%s" % (nome, ip, data.strftime("%m/%d/%Y, %H:%M:%S")))
+        obj.stdout.write("%s\t\t%s\t%s" % (nome, ip, data.strftime("%m/%d/%Y, %H:%M:%S")))
 
 
-def remove_user_n(cursor, mydb):
-    show_users(cursor, mydb)
+def remove_user_n(cursor, mydb, obj):
+    show_users(cursor, mydb, obj)
     query_s = "SELECT INET_NTOA(ip) FROM Usuarios WHERE name=%s"
     query_d = "DELETE FROM Usuarios WHERE name=%s"
     user = input("Name: ")
     cursor.execute(query_s, (user, ))
     reply = cursor.fetchall()
     for ip in reply:
-        print(ip)
-        remove_total_ip(cursor, mydb, ip[0])
+        obj.stdout.write(ip[0])
+        remove_total_ip(cursor, mydb, obj, ip=ip[0])
     cursor.execute(query_d, (user,))
     mydb.commit()
 
 
-def remove_user_ip(cursor, mydb):
+def remove_user_ip(cursor, mydb, obj):
     query = "DELETE FROM Usuarios WHERE ip=INET_ATON(%s)"
-    show_users(cursor, mydb)
+    show_users(cursor, mydb, obj)
     ip = input("Ip: ")
-    remove_total_ip(cursor, mydb, ip)
+    remove_total_ip(cursor, mydb, obj, ip=ip)
     cursor.execute(query, (ip,))
     mydb.commit()
 
 
-def search_user(cursor, mydb):
+def search_user(cursor, mydb, obj):
     query = "SELECT name, INET_NTOA(ip) as IP FROM Usuarios WHERE name=%s OR ip=INET_ATON(%s)"
-    show_users(cursor, mydb)
+    show_users(cursor, mydb, obj)
     name = input("Name: ")
     ip = input("Ip: ")
     cursor.execute(query, (name, ip))
     reply = cursor.fetchall()
     for name, ip in reply:
-        print(name, ip)
+        st = name + " " +  ip
+        obj.stdout.write(st)
