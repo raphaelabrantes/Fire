2209c6be456b1d745eea7bdd854db8f0e4e39fc1
raphaelabrantes <antenabr2@gmail.com>
2020-02-19 23:52:59 -0300

    Created a new command for manager to use fire

---
+++ fire/management/commands/firecli.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import authenticate
+import getpass
+from fire.fire import start
+
+class Command(BaseCommand):
+    help='start firecli'
+    def handle(self, *args, **options):
+        user = input("Login: ")
+        password = getpass.getpass(prompt='Password: ', stream=None)
+        if authenticate(request=None, username=user, password=password):
+            self.stdout.write(self.style.SUCCESS("Login OK"))
+            start()
