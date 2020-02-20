ec11a21f5e012de762534765e33675c642452a02
raphaelabrantes <antenabr2@gmail.com>
2020-02-20 18:01:43 -0300

    Changed all output's to firecli object output

--- fire/menus.py
+++ fire/menus.py
@@ -2,20 +2,21 @@ from fire.users import *
 from fire.portas import *
 
 
-def first_menu(cursor, mydb):
+def first_menu(cursor, mydb, obj):
     try:
         while True:
             os.system("clear")
-            print("Menu/")
+            obj.stdout.write("Menu/")
             mensagem = "Chose one option\n1 User Menu\n2 Port menu\nCtrl+c para sair\n"
-            op = input(mensagem)
+            obj.stdout.write(mensagem)
+            op = input()
             if op == "1":
                 os.system("clear")
-                menu_users(cursor, mydb)
+                menu_users(cursor, mydb, obj)
 
             elif op == "2":
                 os.system("clear")
-                menu_portas(cursor, mydb)
+                menu_portas(cursor, mydb, obj)
 
     except KeyboardInterrupt:
         cursor.close()
@@ -23,7 +24,7 @@ def first_menu(cursor, mydb):
         exit()
 
 
-def menu_users(cursor, mydb):
+def menu_users(cursor, mydb, obj):
     switch = {
         "1": show_users,
         "2": search_user,
@@ -34,18 +35,19 @@ def menu_users(cursor, mydb):
     mensagem = "Choose one option\n1 Show all users\n2 Search User Ips\n3 Add User\n4 Remove User by name\n5 Remove " \
                "User by ip\nOnly the first char will count\n"
     while True:
-        print("Menu/User/")
-        op = input(mensagem)
+        obj.stdout.write("Menu/User/")
+        obj.stdout.write(mensagem)
+        op = input()
         if len(op) < 1:
             break
         op = ord(op[0])
         if 48 < op < 54:
-            switch.get(chr(op))(cursor, mydb)
+            switch.get(chr(op))(cursor, mydb, obj)
         else:
             break
 
 
-def menu_portas(cursor, mydb):
+def menu_portas(cursor, mydb, obj):
     switch = {
         "1": show_ports,
         "2": search_port,
@@ -57,12 +59,13 @@ def menu_portas(cursor, mydb):
     mensagem = "Escolha uma opção\n1 Show all ports\n2 Show personalized search\n3 Add port\n4 Remove same ports\n5 " \
                "Remove using ip and port\n6 Remove using only ip\nOnly the first char will count\n"
     while True:
-        print("Menu/Ports")
-        op = input(mensagem)
+        obj.stdout.write("Menu/Ports")
+        obj.stdout.write(mensagem)
+        op = input()
         if len(op) < 1:
             break
         op = ord(op[0])
         if 48 < op < 55:
-            switch.get(chr(op))(cursor, mydb)
+            switch.get(chr(op))(cursor, mydb, obj)
         else:
             break
